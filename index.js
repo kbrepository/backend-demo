f04994a713e3533136e0aab4d07a8c4b3784b3f1
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2021-04-28 11:10:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by web on 11/2/17.
+ */
+
+var nodeServer = require('./node_server');
+// og("From mongo index **********************************************************************************************")
+var mongo = require('./db_connections/mongo_connection');
+nodeServer.server;
+log.info("From mongo index **********************************************************************************************")
+mongo.mongoConnection();
+
